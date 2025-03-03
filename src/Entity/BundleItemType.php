2f76bba5c6fa288911c402cfd05b490013b8f37f
Barbara Losonc <barbara@vallic.com>
2025-03-03 10:27:08 +0100

    Revert entity PHP attributes

--- src/Entity/BundleItemType.php
+++ src/Entity/BundleItemType.php
@@ -3,55 +3,54 @@
 namespace Drupal\commerce_variation_bundle\Entity;
 
 use Drupal\commerce\Entity\CommerceBundleEntityBase;
-use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\Core\Entity\Attribute\ConfigEntityType;
 
 /**
  * Defines the Variation Bundle Item type configuration entity.
+ *
+ * @ConfigEntityType(
+ *   id = "commerce_bundle_item_type",
+ *   label = @Translation("Variation Bundle type"),
+ *   label_collection = @Translation("Variation Bundle types"),
+ *   label_singular = @Translation("variation bundle type"),
+ *   label_plural = @Translation("variation bundle types"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count variation bundle item type",
+ *     plural = "@count variation bundle item types",
+ *   ),
+ *   handlers = {
+ *     "form" = {
+ *       "add" = "Drupal\commerce_variation_bundle\Form\BundleItemTypeForm",
+ *       "edit" = "Drupal\commerce_variation_bundle\Form\BundleItemTypeForm",
+ *       "delete" = "Drupal\Core\Entity\EntityDeleteForm",
+ *     },
+ *     "list_builder" = "Drupal\commerce_variation_bundle\BundleItemTypeListBuilder",
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     }
+ *   },
+ *   admin_permission = "administer commerce_bundle_item_type",
+ *   bundle_of = "commerce_bundle_item",
+ *   config_prefix = "commerce_bundle_item_type",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "add-form" = "/admin/commerce/config/bundle-types/add",
+ *     "edit-form" = "/admin/commerce/config/bundle-types/{commerce_bundle_item_type}/edit",
+ *     "delete-form" = "/admin/commerce/config/bundle-types/{commerce_bundle_item_type}/delete",
+ *     "collection" = "/admin/commerce/config/bundle-types",
+ *     "canonical" = "/admin/commerce/config/bundle-types/{commerce_bundle_item_type}"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "generateTitle",
+ *     "uuid",
+ *   }
+ * )
  */
-#[ConfigEntityType(
-  id: 'commerce_bundle_item_type',
-  label: new TranslatableMarkup('Variation Bundle type'),
-  label_collection: new TranslatableMarkup('Variation Bundle types'),
-  label_singular: new TranslatableMarkup('variation bundle type'),
-  label_plural: new TranslatableMarkup('variation bundle types'),
-  label_count: [
-    'singular' => '@count variation bundle item type',
-    'plural' => '@count variation bundle item types',
-  ],
-  handlers: [
-    'form' => [
-      'add' => 'Drupal\commerce_variation_bundle\Form\BundleItemTypeForm',
-      'edit' => 'Drupal\commerce_variation_bundle\Form\BundleItemTypeForm',
-      'delete' => 'Drupal\Core\Entity\EntityDeleteForm',
-    ],
-    'list_builder' => 'Drupal\commerce_variation_bundle\BundleItemTypeListBuilder',
-    'route_provider' => [
-      'html' => 'Drupal\Core\Entity\Routing\AdminHtmlRouteProvider',
-    ],
-  ],
-  admin_permission: 'administer commerce_bundle_item_type',
-  bundle_of: 'commerce_bundle_item',
-  config_prefix: 'commerce_bundle_item_type',
-  entity_keys: [
-    'id' => 'id',
-    'label' => 'label',
-    'uuid' => 'uuid',
-  ],
-  links: [
-    'add-form' => '/admin/commerce/config/bundle-types/add',
-    'edit-form' => '/admin/commerce/config/bundle-types/{commerce_bundle_item_type}/edit',
-    'delete-form' => '/admin/commerce/config/bundle-types/{commerce_bundle_item_type}/delete',
-    'collection' => '/admin/commerce/config/bundle-types',
-    'canonical' => '/admin/commerce/config/bundle-types/{commerce_bundle_item_type}',
-  ],
-  config_export: [
-    'id',
-    'label',
-    'generateTitle',
-    'uuid',
-  ]
-)]
 class BundleItemType extends CommerceBundleEntityBase implements BundleItemTypeInterface {
 
   /**
